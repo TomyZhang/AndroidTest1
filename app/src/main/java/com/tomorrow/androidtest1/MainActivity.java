d38cf16653244b66b96f2889988655619d0b662d
TomyZhang <33471060+TomyZhang@users.noreply.github.com>
2018-02-11 23:22:15 +0800

    Update MainActivity.java

--- app/src/main/java/com/tomorrow/androidtest1/MainActivity.java
+++ app/src/main/java/com/tomorrow/androidtest1/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //remote develop modify
+        //remote develop modify 2
     }
 }
